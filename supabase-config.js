34cb920f09a129bf9c59cb1e87374873cd664afe
Heyt0n <107354920+Heyt0n@users.noreply.github.com>
2026-05-31 16:43:45 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,8 +1,12 @@
 // ==========================================
 // CONFIGURATION GLOBALE DE LA BASE SUPABASE
 // ==========================================
-const SUPABASE_URL = "https://vyrnkiedotmwrzoigziq.supabase.co/rest/v1/";
+
+// 1. URL racine de ton projet Supabase (sans /rest/v1/)
+const SUPABASE_URL = "https://vyrnkiedotmwrzoigziq.supabase.co";
+
+// 2. Ta clé d'API publique (Clé publiable copiée depuis ton tableau de bord)
 const SUPABASE_ANON_KEY = "sb_publishable_96xOoNLDIl4j_wrJdrdrRA_PfUCetYb";
 
-// Initialisation du client unique Supabase
+// 3. Initialisation du client unique Supabase pour toute l'application
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
