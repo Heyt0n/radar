99ee04c4e9cfb35615b9dc02db22b267377dda44
Heyt0n <107354920+Heyt0n@users.noreply.github.com>
2026-07-26 10:59:48 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,7 +3,7 @@
 // ==========================================
 
 // 1. URL racine de ton projet Supabase (sans /rest/v1/)
-const SUPABASE_URL = "https://https://iikswhmkacwfnraqdsxi.supabase.co";
+const SUPABASE_URL = "https://iikswhmkacwfnraqdsxi.supabase.co";
 
 // 2. Ta clé d'API publique (Clé publiable copiée depuis ton tableau de bord)
 const SUPABASE_ANON_KEY = "sb_publishable_d4Mn70KIvUW03IRkCQM9Gw_Vw9l_REh";
