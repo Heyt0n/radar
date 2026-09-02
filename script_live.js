33de4104ee59145c446099bc92b87ab8c633cde3
Heyt0n <107354920+Heyt0n@users.noreply.github.com>
2026-09-02 15:56:00 +0200

    Update script_live.js

--- script_live.js
+++ script_live.js
@@ -119,7 +119,7 @@ mapContainer.style.background = '#111827'; // Fond pour éviter le blanc au char
 const styleDark = document.createElement('style');
 styleDark.innerHTML = `
     .leaflet-tile-pane {
-        filter: brightness(2) invert(1) contrast(0.5) hue-rotate(200deg) saturate(0.3);
+        filter: brightness(1) invert(1) contrast(0.5) hue-rotate(200deg) saturate(0.9);
     }
 `;
 document.head.appendChild(styleDark);
